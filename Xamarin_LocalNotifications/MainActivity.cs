90a4f33c67441867e36fdbf8a75efb306432b6c1
BlindRD <anotherchannel404@gmail.com>
2016-12-03 14:37:56 +0300

    Добавьте файлы проекта.

---
+++ Xamarin_LocalNotifications/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Xamarin_LocalNotifications
+{
+    [Activity(Label = "Xamarin_LocalNotifications", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
