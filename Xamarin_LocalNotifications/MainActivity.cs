7165a22c663fd7b336573d300571ba9cc99465d0
BlindRD <anotherchannel404@gmail.com>
2016-12-03 15:09:26 +0300

    Finished project

--- Xamarin_LocalNotifications/MainActivity.cs
+++ Xamarin_LocalNotifications/MainActivity.cs
@@ -1,18 +1,45 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace Xamarin_LocalNotifications
 {
     [Activity(Label = "Xamarin_LocalNotifications", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
+        NotificationManager manager;
+        Notification myNotification;
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            // SET NOTIFICATION
+            Notification.Builder myBuilder = new Notification.Builder(this);
+            myBuilder.SetContentTitle("Who is the most crazy pony?");
+            myBuilder.SetContentText("Pinkie Pie!");
+            myBuilder.SetSmallIcon(Resource.Drawable.pony_icon);
+
+            // BUILD THE NOTIFICATION
+            myNotification = myBuilder.Build();
+
+            // SET THE NOTIFICATION MANAGER
+            manager = GetSystemService(Context.NotificationService) as NotificationManager;
+
+            // AND FINALY - NOTIFY!
+            Button myBtn = FindViewById<Button>(Resource.Id.button1);
+            myBtn.Click += NotifyOnClick;
+        }
+
+        private void NotifyOnClick(object sender, System.EventArgs e)
+        {
+            manager.Notify(0,myNotification);
         }
     }
 }
--- Xamarin_LocalNotifications/Resources/Resource.Designer.cs
+++ Xamarin_LocalNotifications/Resources/Resource.Designer.cs
@@ -47,12 +47,31 @@ namespace Xamarin_LocalNotifications
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int pony_icon = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int button1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
